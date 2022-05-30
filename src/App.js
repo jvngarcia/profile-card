2ce04223cb8c3085bdd5a74a20874421f5fc209c
Angel Garcia <angelgarciaweb@gmail.com>
2022-05-30 12:22:50 -0400

    Solucion de errores

--- src/App.js
+++ src/App.js
@@ -9,36 +9,36 @@ import iconWeb from './assets/images/newspaper-regular.svg';
 // PERFIL
 import profile from './assets/images/perfil.png';
 import banner from './assets/images/banner-jvngarcia-profile.png';
-import background from './assets/images/background.png';
+
 
 function App() {
   return (
-    <Card cardBoxProfile={ profile } cardBoxBanner={ banner }  background={ background }>
+    <Card cardBoxProfile={ profile } cardBoxBanner={ banner }>
       <h4 className='text-center title-padding'>Angel Garcia</h4>
       <h6 className='text-center subtitle-padding'>Programador</h6>
       <div className='card__box__container'>
         <div className='redes-sociales'>
-          <a href='#'  className='text-center contenedor-items'>
+          <a href='https://www.instagram.com/jvngarcia_/' target='_blank' rel="noreferrer"  className='text-center contenedor-items'>
             <div>
-              <div className='contenedor-imagen'><img src={ iconInstagram }/></div>
+              <div className='contenedor-imagen'><img src={ iconInstagram } alt='icono de Instagram' /></div>
               Instagram
             </div>
           </a>
-          <a href='#' className='text-center contenedor-items'>
+          <a href='https://www.tiktok.com/@jvngarcia' target='_blank' rel="noreferrer" className='text-center contenedor-items'>
             <div>
-              <div className='contenedor-imagen'><img src={ iconTiktok }/></div>
+              <div className='contenedor-imagen'><img src={ iconTiktok } alt='icono de Tiktok' /></div>
               Tiktok
             </div>
           </a>
-          <a href='#' className='text-center contenedor-items'>
+          <a href='https://www.linkedin.com/in/jvngarcia/' target='_blank' rel="noreferrer" className='text-center contenedor-items'>
             <div>
-              <div className='contenedor-imagen'><img src={ iconLinkedin }/></div>
+              <div className='contenedor-imagen'><img src={ iconLinkedin } alt='icono de Linkedin' /></div>
               LinkedIn
             </div>
           </a>
-          <a href='#' className='text-center contenedor-items'>
+          <a href='https://jvngarcia.com/' className='text-center contenedor-items'>
             <div>
-              <div className='contenedor-imagen'><img src={ iconWeb }/></div>
+              <div className='contenedor-imagen'><img src={ iconWeb } alt='icono de mi sitio web' /></div>
               Sitio Web
             </div>
           </a>
